0233bf544c27c1d20643758a7d1076d30b583aef
jy4education_git <jy4education@gmail.com>
2026-01-11 22:07:58 +0100

    Übungen zu Quote Switching und Verwendungen von Backticks hinzugefügt

--- code.js
+++ code.js
@@ -20,12 +20,6 @@ var firstName = "John";
 var lastName = "Doe";
 var age = 30;
 
-console.log("My name is " + firstName + " " + lastName + " and I am " + age + " years old.");
-console.log("My name is ${firstName} ${lastName} and I am ${age} years old.");
-console.log(`My name is ${firstName} ${lastName} and I am ${age} years old.`);
-console.log(`My name is ${firstName} ${lastName} 
-and I am ${age} years old.`);
-console.log("My name is " + firstName + " " + lastName + "\n" + "and I am " + age + " years old.");
 /*
 || VERGLEICH: STRING BEGRENZER IN JAVASCRIPT
 ||--------------------------------------------------------------------------------------------------------||
@@ -36,3 +30,34 @@ console.log("My name is " + firstName + " " + lastName + "\n" + "and I am " + ag
 || Interpolation || Nein (nur über +)            || Ja (via ${variable})        || Nein (SyntaxError)     ||
 || Mehrzeilig    || Nur mit \n                   || Ja (nativ durch Enter)      || Nein                   ||
 */
+
+// console.log("My name is " + firstName + " " + lastName + " and I am " + age + " years old.");
+// console.log("My name is ${firstName} ${lastName} and I am ${age} years old.");
+// console.log(`My name is ${firstName} ${lastName} and I am ${age} years old.`);
+// console.log(`My name is ${firstName} ${lastName} 
+// and I am ${age} years old.`);
+// console.log("My name is " + firstName + " " + lastName + "\n" + "and I am " + age + " years old.");
+
+// Schlechte Verwendung von Escaped Quotes:
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"";
+
+// Wirkt sehr unelegant, besser mit „Quote-Switching“: 
+// Wie erzeuge ich „“ ?: Windows : Alt + 0132 (auf dem Ziffernblock) Alt + 0147 (auf dem Ziffernblock)
+var myStr2 = 'I am a "double quoted" string inside "double quotes"';
+console.log("myStr: " + myStr);
+console.log("myStr2: " + myStr2);
+// Ausgabe:
+// I am a "double quoted" string inside "double quotes"
+// I am a "double quoted" string inside "double quotes"
+
+// Best Practice über Backticks:
+var myStr3 = `I am a "double quoted" string inside "double quotes"`;
+var myStr4 = `Er sagte: "Es ist John's Auto", und ging.`;
+console.log("myStr3: " + myStr3);
+console.log("myStr4: " + myStr4);
+// Ausgabe:
+// I am a "double quoted" string inside "double quotes"
+// Er sagte: "Es ist John's Auto", und ging.
+console.log(`"Das war ein langer Tag...", sagte er nachdenklich.
+"Ich bin müde." 
+Dann ging er nach Hause.`);
